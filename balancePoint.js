7eee2bf874c0db12177a34b3c205a3e42eee00bc
Sharol Chand <sharolchand@gmail.com>
2016-05-31 15:21:15 -0700

    balancePoint.js

---
+++ balancePoint.js
@@ -0,0 +1,17 @@
+function balacePoint(arr){
+	for(var i = 0; i < arr.length; i++){
+		var left = 0;
+		var right = 0;
+		for(var j = 0; j < arr.length; j++){
+			if(j > i){
+				right += arr[j];
+			}else{
+				left += arr[j];
+			}
+		}
+		if(left == right){
+			return true;
+		}
+	}
+	return false;
+}
