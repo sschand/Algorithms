1a7aba13d8a0f8a9e7ef545c61c25b106bac3654
Sharol Chand <sharol_chand@apple.com>
2018-05-17 16:14:39 -0700

    added maxSpan

---
+++ array_maxSpan.js
@@ -0,0 +1,6 @@
+/*
+Consider the leftmost and righmost appearances of some value in an array.
+We'll say that the "span" is the number of elements between the two inclusive.
+A single value has a span of 1. Returns the largest span found in the given array.
+(Efficiency is not a priority.)
+ */
