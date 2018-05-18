85dedeafbae181a186d6d6e0eb132b606f6cc7a7
Sharol Chand <sharol_chand@apple.com>
2018-05-18 14:22:32 -0700

    updated function names to reflect name approriately

--- balanceIndex.js
+++ balanceIndex.js
@@ -1,4 +1,4 @@
-function balaceIndex(arr){
+function balanceIndex(arr){
 	for(var i = 0; i < arr.length; i++){
 		var left = 0;
 		var right = 0;
--- balancePoint.js
+++ balancePoint.js
@@ -1,4 +1,4 @@
-function balacePoint(arr){
+function balancePoint(arr){
 	for(var i = 0; i < arr.length; i++){
 		var left = 0;
 		var right = 0;
